5a68338c208b325b58dc82f0af5202b83195345a
DebeshMishra <98782809+DebeshMishra@users.noreply.github.com>
2022-04-28 14:03:15 -0700

    Save implemented
    Save working as discussed with Hassan. Load code is present, just need to write the actual loading logic.

--- src/GUI/Grid.java
+++ src/GUI/Grid.java
@@ -1,19 +1,24 @@
 package GUI;
 
 import javax.swing.BorderFactory;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.geom.Ellipse2D;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 
 public class Grid extends JPanel {
@@ -22,6 +27,8 @@ public class Grid extends JPanel {
 	private static final int DOT_DIAMETER = 5;
 
 	private static ArrayList<Dot> dotsList = new ArrayList<>();
+	
+	private static final int[][] xyPoints = new int[720][720];
 
 	private static void generateDots(Graphics2D g2){
 		for (int i = 0; i < MAX_DOTS; i++){
@@ -74,13 +81,73 @@ public class Grid extends JPanel {
         JMenuItem exit = new JMenuItem("Exit");
         file.add(exit);
 
-        class exitAction implements ActionListener {
+        class action implements ActionListener {
             public void actionPerformed (ActionEvent e) {
-                System.exit(0);
+            	if(e.getSource()==exit)
+            		System.exit(0);
+            	if(e.getSource()==save) {
+            		JFileChooser fileChooser = new JFileChooser();
+            		int response = fileChooser.showSaveDialog(null);
+            		   
+            		if(response == JFileChooser.APPROVE_OPTION) {
+            		    File file;
+            		    PrintWriter fileOut = null;
+            		    
+            		    file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+            		    try {
+            		    	fileOut = new PrintWriter(file);
+            		        
+            		        String saveStr = new String();
+            		        
+            		        for(int[] a: xyPoints) {
+            		        	saveStr +=a[0]+","+a[1]+"\n";
+            		        }
+            		    	
+            		    	fileOut.println(saveStr);
+            		    } 
+            		    catch (FileNotFoundException e1) {
+            		    	// TODO Auto-generated catch block
+            		    	e1.printStackTrace();
+            		    }
+            		    finally {
+            		    	fileOut.close();
+            		    } 
+            		}
+            	}
+            	if(e.getSource()==load) {
+            		JFileChooser fileChooser = new JFileChooser();
+            		fileChooser.setCurrentDirectory(new File("."));
+            		FileNameExtensionFilter filter = new FileNameExtensionFilter("Text files", "txt");
+            		fileChooser.setFileFilter(filter);
+            		   
+            		int response = fileChooser.showOpenDialog(null);
+            		   
+            		if(response == JFileChooser.APPROVE_OPTION) {
+            		    File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+            		    Scanner fileIn = null;
+            		    
+            		    try {
+            		    	fileIn = new Scanner(file);
+            		    	if(file.isFile()) {
+            		    		while(fileIn.hasNextLine()) {
+            		    			String line = fileIn.nextLine()+"\n";
+            		    			//textArea.append(line);
+            		    		}
+            		    	}
+            		    } catch (FileNotFoundException e1) {
+            		     // TODO Auto-generated catch block
+            		    	e1.printStackTrace();
+            		    }
+            		    finally {
+            		    	fileIn.close();
+            		    }
+            		}
+            	}
             }
         }
         
-        exit.addActionListener(new exitAction());
+        exit.addActionListener(new action());
+        save.addActionListener(new action());
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Grid");
